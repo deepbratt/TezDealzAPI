2501740781bb669a55866fda66437836ca98bb44
Hamza Saif <hamzasaiff@gmail.com>
2021-08-10 15:17:37 +0500

    Updated Users API and Updated Swagger doc for Users

--- users/controller/auth/accountVerification.js
+++ users/controller/auth/accountVerification.js
@@ -2,86 +2,142 @@ const crypto = require('crypto');
 const User = require('../../model/userModel');
 const AppError = require('@utils/tdb_globalutils/errorHandling/AppError');
 const catchAsync = require('@utils/tdb_globalutils/errorHandling/catchAsync');
-const { ERRORS, STATUS_CODE, SUCCESS_MSG, STATUS } = require('@constants/tdb-constants');
+const {
+  ERRORS,
+  STATUS_CODE,
+  SUCCESS_MSG,
+  STATUS,
+} = require('@constants/tdb-constants');
 const Email = require('../../utils/email');
 const sendSMS = require('../../utils/sendSMS');
 
 exports.sendVerificationCodetoPhone = async (req, res, next) => {
-	const user = await User.findOne({ phone: req.body.phone });
-	if (!user) {
-		return next(new AppError(ERRORS.INVALID.NOT_FOUND, STATUS_CODE.NOT_FOUND));
-	}
-	const verificationToken = await user.accountVerificationToken();
-	// console.log(resetToken);
-	await user.save({ validateBeforeSave: false });
+  const user = await User.findOne({ phone: req.body.phone });
+  if (!user) {
+    return next(new AppError(ERRORS.INVALID.NOT_FOUND, STATUS_CODE.NOT_FOUND));
+  }
+  const verificationToken = await user.phoneVerificationToken();
+  // console.log(resetToken);
+  await user.save({ validateBeforeSave: false });
 
-	try {
-		await sendSMS({
-			body: `Your TezDealz account verification code is ${verificationToken}`,
-			phone: user.phone, // Text this number
-			from: process.env.TWILIO_PHONE_NUMBER, // From a valid Twilio number
-		});
+  try {
+    await sendSMS({
+      body: `Your TezDealz account verification code is ${verificationToken}`,
+      phone: user.phone, // Text this number
+      from: process.env.TWILIO_PHONE_NUMBER, // From a valid Twilio number
+    });
 
-		res.status(STATUS_CODE.OK).json({
-			status: STATUS.UNVERIFIED,
-			message: SUCCESS_MSG.SUCCESS_MESSAGES.TOKEN_SENT_PHONE,
-		});
-	} catch (err) {
-		user.accountVeificationToken = undefined;
-		user.verificationTokenExpires = undefined;
-		await user.save({ validateBeforeSave: false });
-		return next(new AppError(ERRORS.RUNTIME.SENDING_MESSAGE), STATUS_CODE.SERVER_ERROR);
-	}
+    res.status(STATUS_CODE.OK).json({
+      status: STATUS.UNVERIFIED,
+      message: SUCCESS_MSG.SUCCESS_MESSAGES.TOKEN_SENT_PHONE,
+    });
+  } catch (err) {
+    user.phoneVerificationCode = undefined;
+    user.phoneVerificationTokenExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(
+      new AppError(ERRORS.RUNTIME.SENDING_MESSAGE),
+      STATUS_CODE.SERVER_ERROR,
+    );
+  }
 };
 
 exports.sendVerificationCodetoEmail = async (req, res, next) => {
-	const user = await User.findOne({ email: req.body.email });
+  const user = await User.findOne({ email: req.body.email });
 
-	if (!user) {
-		return next(new AppError(ERRORS.INVALID.NOT_FOUND), STATUS_CODE.NOT_FOUND);
-	}
+  if (!user) {
+    return next(new AppError(ERRORS.INVALID.NOT_FOUND), STATUS_CODE.NOT_FOUND);
+  }
 
-	const verificationToken = await user.accountVerificationToken();
-	await user.save({ validateBeforeSave: false });
+  const verificationToken = await user.emailVerificationToken();
+  await user.save({ validateBeforeSave: false });
 
-	try {
-		// await sendSMS({
-		// 	body: `Your TezDealz account verification code is ${verificationToken}`,
-		// 	phone: user.phone, // Text this number
-		// 	from: process.env.TWILIO_PHONE_NUMBER, // From a valid Twilio number
-		// });
+  try {
+    // await sendSMS({
+    // 	body: `Your TezDealz account verification code is ${verificationToken}`,
+    // 	phone: user.phone, // Text this number
+    // 	from: process.env.TWILIO_PHONE_NUMBER, // From a valid Twilio number
+    // });
 
-		res.status(STATUS_CODE.OK).json({
-			status: STATUS.UNVERIFIED,
-			message: SUCCESS_MSG.SUCCESS_MESSAGES.TOKEN_SENT_EMAIL,
-		});
-	} catch (err) {
-		user.accountVeificationToken = undefined;
-		user.verificationTokenExpires = undefined;
-		await user.save({ validateBeforeSave: false });
-		return next(new AppError(ERRORS.RUNTIME.SENDING_MESSAGE), STATUS_CODE.SERVER_ERROR);
-	}
+    res.status(STATUS_CODE.OK).json({
+      status: STATUS.UNVERIFIED,
+      message: SUCCESS_MSG.SUCCESS_MESSAGES.TOKEN_SENT_EMAIL,
+    });
+  } catch (err) {
+    user.emailVerificationCode = undefined;
+    user.emailVerificationTokenExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(
+      new AppError(ERRORS.RUNTIME.SENDING_MESSAGE),
+      STATUS_CODE.SERVER_ERROR,
+    );
+  }
 };
 
-exports.accountVerification = catchAsync(async (req, res, next) => {
-	const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
-	//const hashedToken = req.params.token;
+// Phone verification
+exports.phonetVerification = catchAsync(async (req, res, next) => {
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+  //const hashedToken = req.params.token;
 
-	const user = await User.findOne({
-		accountVeificationToken: hashedToken,
-		verificationTokenExpires: { $gt: Date.now() },
-	});
+  const user = await User.findOne({
+    phoneVerificationCode: hashedToken,
+    phoneVerificationTokenExpires: { $gt: Date.now() },
+  });
 
-	if (!user) {
-		return next(new AppError(ERRORS.INVALID.INVALID_VERIFICATION_TOKEN), STATUS_CODE.UNAUTHORIZED);
-	}
+  if (!user) {
+    return next(
+      new AppError(ERRORS.INVALID.INVALID_VERIFICATION_TOKEN),
+      STATUS_CODE.UNAUTHORIZED,
+    );
+  }
 
-	user.isVerified = true;
-	user.accountVeificationToken = undefined;
-	user.verificationTokenExpires = undefined;
-	await user.save();
-	res.status(STATUS_CODE.OK).json({
-		status: STATUS.SUCCESS,
-		message: SUCCESS_MSG.SUCCESS_MESSAGES.ACCOUNT_VERIFICATION,
-	});
+  user.isVerified = true;
+  user.isPhoneVerified = true;
+  user.loggedInWithPhone = true;
+  // user.isEmailVerified = undefined;
+  // user.loggedInWithEmail = undefined;
+  user.phoneVerificationCode = undefined;
+  user.phoneVerificationTokenExpires = undefined;
+  await user.save();
+  res.status(STATUS_CODE.OK).json({
+    status: STATUS.SUCCESS,
+    message: SUCCESS_MSG.SUCCESS_MESSAGES.ACCOUNT_VERIFICATION,
+  });
+});
+
+// email Verification code
+exports.emailVerification = catchAsync(async (req, res, next) => {
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+  //const hashedToken = req.params.token;
+
+  const user = await User.findOne({
+    emailVerificationCode: hashedToken,
+    emailVerificationTokenExpires: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      new AppError(ERRORS.INVALID.INVALID_VERIFICATION_TOKEN),
+      STATUS_CODE.UNAUTHORIZED,
+    );
+  }
+
+  user.isVerified = true;
+  user.isEmailVerified = true;
+  user.loggedInWithEmail = true;
+  // user.isPhoneVerified = undefined;
+  // user.loggedInWithPhone = undefined;
+  user.emailVerificationCode = undefined;
+  user.emailVerificationTokenExpires = undefined;
+  await user.save();
+  res.status(STATUS_CODE.OK).json({
+    status: STATUS.SUCCESS,
+    message: SUCCESS_MSG.SUCCESS_MESSAGES.ACCOUNT_VERIFICATION,
+  });
 });
--- users/utils/email.js
+++ users/utils/email.js
@@ -1,39 +1,41 @@
-const mailchimp = require('@mailchimp/mailchimp_transactional')('wBBQ_gkehEEbjxn5E-LGWg');
+const mailchimp = require('@mailchimp/mailchimp_transactional')(
+  'wBBQ_gkehEEbjxn5E-LGWg',
+);
 
 module.exports = class Email {
-	constructor(user, resetToken) {
-		this.to = user.email;
-		this.firstName = user.firstName;
-		this.lastName = user.lastName;
-		this.resetToken = resetToken;
-	}
-	async forgotPassword() {
-		const response = await mailchimp.messages.sendTemplate({
-			template_name: 'passwordresettoken',
-			template_content: [
-				{
-					name: 'fname',
-					content: this.firstName,
-				},
-				{
-					name: 'resetToken',
-					content: this.resetToken,
-				},
-			],
-			message: {
-				subject: 'Password Reset Token Valid for 30 minutes',
-				from_email: 'info@themagnit.com',
-				from_name: 'TezDeal',
-				to: [
-					{
-						email: this.to,
-						name: this.firstName,
-					},
-				],
-			},
-		});
-		return response;
-	}
+  constructor(user, resetToken) {
+    this.to = user.email;
+    this.firstName = user.firstName;
+    this.lastName = user.lastName;
+    this.resetToken = resetToken;
+  }
+  async forgotPassword() {
+    const response = await mailchimp.messages.sendTemplate({
+      template_name: 'passwordresettoken',
+      template_content: [
+        {
+          name: 'fname',
+          content: this.firstName,
+        },
+        {
+          name: 'resetToken',
+          content: this.resetToken,
+        },
+      ],
+      message: {
+        subject: 'Password Reset Token Valid for 30 minutes',
+        from_email: 'info@themagnit.com',
+        from_name: 'TezDeal',
+        to: [
+          {
+            email: this.to,
+            name: this.firstName,
+          },
+        ],
+      },
+    });
+    return response;
+  }
 };
 
 // const sendEmail = async (options) => {
