1b135d30e9176788182a0c4162438e2e5140fac5
Azeem Haider <68206777+Azeem-Haider@users.noreply.github.com>
2021-08-13 01:47:09 +0500

    updated

--- ads/contollers/factoryHandler.js
+++ ads/contollers/factoryHandler.js
@@ -1,9 +1,11 @@
-const { AppError, catchAsync, uploadS3, APIFeatures } = require('@utils/tdb_globalutils');
+const { APIFeatures } = require('@utils/tdb_globalutils');
 
 exports.filter = async (query, queryParams) => {
-	const freatures = new APIFeatures(query, queryParams).filter().sort().limitFields().pagination();
 	const results = new APIFeatures(query, queryParams).filter().sort().limitFields();
-	const doc = await freatures.query;
 	const totalCount = await results.query.count();
+
+	const freatures = new APIFeatures(query, queryParams).filter().sort().limitFields().pagination();
+	const doc = await freatures.query;
+
 	return [doc, totalCount];
 };
