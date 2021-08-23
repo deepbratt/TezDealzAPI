61d10149c583c3541751c80fa780fd2864f517a6
Azeem Haider <68206777+Azeem-Haider@users.noreply.github.com>
2021-08-23 17:41:40 +0500

    updated ads

--- ads/contollers/carController.js
+++ ads/contollers/carController.js
@@ -45,15 +45,8 @@ exports.createOne = catchAsync(async (req, res, next) => {
 });
 
 exports.getAll = catchAsync(async (req, res, next) => {
-	let result, totalCount;
-	if (req.user && req.user.role != 'User') {
-		[result, totalCount] = await filter(Car.find(), req.query);
-	} else {
-		[result, totalCount] = await filter(
-			Car.find({ active: true, isSold: false, banned: false }),
-			req.query
-		);
-	}
+	const [result, totalCount] = await filter(Car.find(), req.query);
+
 	if (result.length === 0) {
 		return next(new AppError(ERRORS.INVALID.NOT_FOUND, STATUS_CODE.NOT_FOUND));
 	}
