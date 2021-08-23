6333c119b94d6dfecf4d0d25e4ff956ecc70480f
Azeem Haider <68206777+Azeem-Haider@users.noreply.github.com>
2021-08-23 20:18:23 +0500

    updated ads

--- ads/contollers/factoryHandler.js
+++ ads/contollers/factoryHandler.js
@@ -59,7 +59,7 @@ exports.dailyAggregate = (Model) => {
 			},
 			{
 				$group: {
-					_id: { $dateToString: { format: '%Y-%M-%D', date: '$createdAt' } },
+					_id: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt' } },
 					cars: { $sum: 1 },
 				},
 			},
