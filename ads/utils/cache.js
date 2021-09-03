ccdcbb84b709d7ddd6ef3e28b0ddd1387fd37c9a
Azeem Haider <68206777+Azeem-Haider@users.noreply.github.com>
2021-09-03 17:55:47 +0500

    redis cache update

--- ads/utils/cache.js
+++ ads/utils/cache.js
@@ -35,7 +35,7 @@ module.exports = (duration) => async (req, res, next) => {
 
 	if (cachedResponse) {
 		console.log(`Cache hit for ${key}`);
-		res.json(cachedResponse);
+		res.json(JSON.stringify(cachedResponse));
 	} else {
 		console.log(`Cache miss for ${key}`);
 		res.originalSend = res.json;
