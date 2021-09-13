6e9c077b0daf6b1b346a680b1c3430dd639331f0
Azeem Haider <68206777+Azeem-Haider@users.noreply.github.com>
2021-09-13 21:31:56 +0500

    tickets service added

--- tickets/models/user/userModel.js
+++ tickets/models/user/userModel.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcryptjs = require('bcryptjs');
-const crypto = require('crypto');
 const { ERRORS } = require('@constants/tdb-constants');
 
 const userSchema = new mongoose.Schema(
@@ -160,51 +158,6 @@ const userSchema = new mongoose.Schema(
 	}
 );
 
-//indexes
-userSchema.index({ email: 1 }, { unique: true, sparse: true });
-userSchema.index({ phone: 1 }, { unique: true, sparse: true });
-userSchema.index({ username: 1 }, { unique: true, sparse: true });
-
-userSchema.index({
-	firstName: 'text',
-	lastName: 'text',
-	email: 'text',
-	username: 'text',
-});
-
-//pre save middleware (runs before data saved to db)
-userSchema.pre('save', async function (next) {
-	if (!this.isModified('password')) return next();
-	this.password = await bcryptjs.hash(this.password, 12);
-	this.passwordConfirm = undefined;
-	next();
-});
-
-// to check if user change password
-userSchema.pre('save', function (next) {
-	if (!this.isModified('password') || this.isNew) return next();
-	this.passwordChangedAt = Date.now();
-	next();
-});
-
-// To show only active true users in Users.find() query
-// userSchema.pre(/^find/, function (next) {
-//   // /^find/ find all that startsWith (find)
-//   // this. points to current query
-//   this.find({
-//     active: {
-//       $ne: false,
-//     },
-//   });
-//   next();
-// });
-
-//SCHEMA METHODS
-userSchema.methods.correctPassword = async function (candidatePassword, userpassword) {
-	// Check Password Is Correct??
-	return await bcryptjs.compare(candidatePassword, userpassword);
-};
-
 //CHANGED_PASSWORD_AFTER
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 	if (this.passwordChangedAt) {
@@ -213,51 +166,6 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 	}
 	return false;
 };
-
-// Instance Method to get Random 4-digit code
-userSchema.methods.createPasswordResetToken = async function () {
-	let resetToken;
-	do {
-		resetToken = Math.floor(Math.random() * (1000 - 9999 + 1) + 9999).toString();
-	} while (
-		await User.findOne({
-			passwordResetToken: crypto.createHash('sha256').update(resetToken).digest('hex'),
-		})
-	);
-	this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
-	this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-	return resetToken;
-};
-
-// Email Verification Token
-// userSchema.methods.emailVerificationToken = async function () {
-//   let verificationToken;
-//   do {
-//     verificationToken = Math.floor(Math.random() * (100000 - 999999 + 1) + 999999).toString();
-//   } while (
-//     await User.findOne({
-//       emailVerificationCode: crypto.createHash('sha256').update(verificationToken).digest('hex'),
-//     })
-//   );
-//   this.emailVerificationCode = crypto.createHash('sha256').update(verificationToken).digest('hex');
-//   this.emailVerificationTokenExpires = Date.now() + 10 * 60 * 1000;
-//   return verificationToken;
-// };
-
-// Phone Verification Token
-// userSchema.methods.phoneVerificationToken = async function () {
-//   let verificationToken;
-//   do {
-//     verificationToken = Math.floor(Math.random() * (100000 - 999999 + 1) + 999999).toString();
-//   } while (
-//     await User.findOne({
-//       phoneVerificationCode: crypto.createHash('sha256').update(verificationToken).digest('hex'),
-//     })
-//   );
-//   this.phoneVerificationCode = crypto.createHash('sha256').update(verificationToken).digest('hex');
-//   this.phoneVerificationTokenExpires = Date.now() + 10 * 60 * 1000;
-//   return verificationToken;
-// };
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
