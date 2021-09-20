19f7c1f0172fc34b99ac48731772b1ca76c3844d
Hamza Saif <hamzasaiff@gmail.com>
2021-09-21 01:27:34 +0500

    added upstash credentials

--- tickets/config/dbConnection.js
+++ tickets/config/dbConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const db = process.env.NODE_ENV === 'production' ? process.env.DB_LOCAL : process.env.DB_LOCAL;
+const db = process.env.NODE_ENV === 'production' ? process.env.DB_LOCAL : process.env.DB_REMOTE;
 
 const dbConnect = async () => {
   try {
