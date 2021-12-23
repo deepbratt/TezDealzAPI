8e7ae7a28fda5cb4ed3d7aea1debec47e080eb23
Hamza Saif <hamzasaiff@gmail.com>
2021-12-23 14:37:34 +0500

    Added data of cities and states in dev database and updated controllers and swagger

--- ads/contollers/country-city-state-province/countryCityState.js
+++ ads/contollers/country-city-state-province/countryCityState.js
@@ -1,51 +1,50 @@
-const city = require('./data/city.json');
-const state = require('./data/state.json');
-
-var _ = require("underscore");
-
+const Cities = require('../../models/city-states/citiesModel');
+const States = require('../../models/city-states/statesModel');
+const { filter } = require('../factory/factoryHandler');
 const { AppError, catchAsync } = require('@utils/tdb_globalutils');
 const { STATUS, STATUS_CODE, SUCCESS_MSG, ERRORS } = require('@constants/tdb-constants');
 
 exports.getAllStates = catchAsync(async (req, res, next) => {
-
-  var result = state;
+  const [result, totalCount] = await filter(States.find(), req.query);
 
   if (result[0].length <= 0) {
     return next(new AppError(ERRORS.INVALID.NOT_FOUND, STATUS_CODE.NOT_FOUND));
   }
+  console.log(result.length);
 
   res.status(200).json({
     status: STATUS.SUCCESS,
+    message: 'List of all States',
+    countOnPage: result.length,
+    totalCount: totalCount,
     data: {
-      result
+      result,
     },
   });
 });
-
 
 exports.getStateByCode = catchAsync(async (req, res, next) => {
+  const result = await States.find({ isoCode: req.params.stateCode });
 
-  var result = _.where(state, { isoCode: req.params.stateCode });
-
-  if (result[0].length <= 0) {
+  if (result.length <= 0) {
     return next(new AppError(ERRORS.INVALID.NOT_FOUND, STATUS_CODE.NOT_FOUND));
   }
 
   res.status(200).json({
     status: STATUS.SUCCESS,
+    message: 'List of all states by code',
+    countOnPage: result.length,
     data: {
-      result
+      result,
     },
   });
 });
 
-
 exports.getStateByCodeAndCountry = catchAsync(async (req, res, next) => {
-
   var state_code = req.params.stateCode;
   var country_code = req.params.countryCode;
 
-   var result = _.where(state, { countryCode: country_code,  isoCode:state_code});
+  const result = await States.find({ countryCode: country_code, isoCode: state_code });
 
   if (result[0].length <= 0) {
     return next(new AppError(ERRORS.INVALID.NOT_FOUND, STATUS_CODE.NOT_FOUND));
@@ -53,31 +52,37 @@ exports.getStateByCodeAndCountry = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: STATUS.SUCCESS,
+    message: 'State data by state and country code',
+    countOnPage: result.length,
     data: {
-      result
+      result,
     },
   });
 });
 
 exports.getStatesOfCountry = catchAsync(async (req, res, next) => {
+  const [result, totalCount] = await filter(
+    States.find({ countryCode: req.params.countryCode }),
+    req.query,
+  );
 
-  var result = _.where(state, { countryCode: req.params.countryCode });
-
-  if (result[0].length <= 0) {
+  if (result.length <= 0) {
     return next(new AppError(ERRORS.INVALID.NOT_FOUND, STATUS_CODE.NOT_FOUND));
   }
 
   res.status(200).json({
     status: STATUS.SUCCESS,
+    message: 'List of all states by country code',
+    countOnPage: result.length,
+    totalCount: totalCount,
     data: {
-      result
+      result,
     },
   });
 });
 
 exports.getAllCities = catchAsync(async (req, res, next) => {
-
-  var result = city;
+  const [result, totalCount] = await filter(Cities.find(), req.query);
 
   if (result[0].length <= 0) {
     return next(new AppError(ERRORS.INVALID.NOT_FOUND, STATUS_CODE.NOT_FOUND));
@@ -85,17 +90,23 @@ exports.getAllCities = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: STATUS.SUCCESS,
+    message: 'List of all cities',
+    countOnPage: result.length,
+    totalCount: totalCount,
     data: {
-      result
+      result,
     },
   });
 });
 
 exports.getCitiesOfState = catchAsync(async (req, res, next) => {
-
   var countryCode_f = req.params.countryCode;
   var stateCode_f = req.params.stateCode;
-  var result = _.where(city, { stateCode: stateCode_f, countryCode:countryCode_f})
+
+  const [result, totalCount] = await filter(
+    Cities.find({ stateCode: stateCode_f, countryCode: countryCode_f }),
+    req.query,
+  );
 
   if (result[0].length <= 0) {
     return next(new AppError(ERRORS.INVALID.NOT_FOUND, STATUS_CODE.NOT_FOUND));
@@ -103,16 +114,17 @@ exports.getCitiesOfState = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: STATUS.SUCCESS,
+    message: 'All cities of states',
+    countOnPage: result.length,
+    totalCount: totalCount,
     data: {
-      result
+      result,
     },
   });
 });
 
 exports.getCitiesOfCountry = catchAsync(async (req, res, next) => {
-
-  
-  var result = _.where(city, { countryCode: "PK" });
+  const [result, totalCount] = await filter(Cities.find({ countryCode: 'PK' }), req.query);
 
   if (result[0].length <= 0) {
     return next(new AppError(ERRORS.INVALID.NOT_FOUND, STATUS_CODE.NOT_FOUND));
@@ -120,10 +132,11 @@ exports.getCitiesOfCountry = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: STATUS.SUCCESS,
+    message: 'All cities of country',
+    countOnPage: result.length,
+    totalCount: totalCount,
     data: {
-      result
+      result,
     },
   });
 });
-
-// console.log(_.where(city,{name:x}))
