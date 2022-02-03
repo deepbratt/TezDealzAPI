d338eb2fa3e4c4a78df12acf9046ed761364ba5e
HSR Hi-Tech Solutions <rajh91774@gmail.com>
2022-02-03 22:18:33 +0530

    Appointment schema change

--- ads/index.js
+++ ads/index.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cluster = require('cluster');
-const os = require('os');
-const morgan = require('morgan');
-const cors = require('cors');
-const session = require('cookie-session');
-const compression = require('compression');
-dotenv.config({ path: './config/config.env' }); // read config.env to environmental variables
-require('./config/dbConnection')(); // db connection
+const express = require("express");
+const dotenv = require("dotenv");
+const cluster = require("cluster");
+const os = require("os");
+const morgan = require("morgan");
+const cors = require("cors");
+const session = require("cookie-session");
+const compression = require("compression");
+dotenv.config({ path: "./config/config.env" }); // read config.env to environmental variables
+require("./config/dbConnection")(); // db connection
 const numCpu = os.cpus().length;
 const PORT = 3003; // port
 
-const { errorHandler, AppError } = require('@utils/tdb_globalutils');
+const { errorHandler, AppError } = require("@utils/tdb_globalutils");
 
-const receivers = require('./utils/rabbitMq');
+const receivers = require("./utils/rabbitMq");
 
-const adsRoutes = require('./constants/consts').routeConsts.carRoutes;
-const adsRouter = require('./routes/carRoutes');
+const adsRoutes = require("./constants/consts").routeConsts.carRoutes;
+const adsRouter = require("./routes/carRoutes");
 
 const app = express();
 
 // CORS
 app.use(cors());
-app.options('*', cors());
-app.use(morgan('dev'));
+app.options("*", cors());
+app.use(morgan("dev"));
 
 // GLOBAL MIDDLEWARES
 app.use(express.json()); // body parser (reading data from body to req.body)
@@ -38,7 +38,7 @@ app.use(compression());
 //routes
 receivers.userbanReceiver();
 app.use(adsRoutes, adsRouter);
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
 	next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
 });
 
