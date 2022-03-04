cd1beea7859ec71a1bc2c309ee62034001c2f6ef
HSR Hi-Tech Solutions <rajh91774@gmail.com>
2022-03-04 23:24:16 +0530

    added few fields in Ratings

--- ecommerce/controller/ratings.js
+++ ecommerce/controller/ratings.js
@@ -32,12 +32,13 @@ exports.addReview = catchAsync(async(req,res,next)=>{
       //     { "_id": pId },
       //     { $inc: {key: 1 } }
       //  )
-      let result = Product.findByIdAndUpdate(pId, {$inc:{"ratingsCount.count3": 1}}, {new: true})
-       if(result.err){
-        //  Error
-        console.log("Error")
-       }
-       console.log(result)
+      // let result = Product.update({"_id": pId}, {$inc:{"ratingsCount.count3": 1}}, {new: true})
+      //  if(result.err){
+      //   //  Error
+      //   console.log("Error")
+      //  }
+      //  console.log(result)
+
         let ratings = await Ratings.create(req.body);
         if (ratings.err) { console.log('error');}
         else { 
@@ -53,6 +54,42 @@ exports.addReview = catchAsync(async(req,res,next)=>{
 
 exports.getRatings = catchAsync(async(req,res,next)=>{
   const result = await (Ratings.find({"productId": req.params.id}));
+  sum =0;
+  let count_1 = 0;
+  let count_2 = 0;
+  let count_3 = 0;
+  let count_4 = 0;
+  let count_5 = 0;
+  for(let x=0; x<result.length; x++){
+    sum = sum + result[x].rating 
+    if(result[x].rating == 1){
+      count_1 = count_1 + 1
+    }else if(result[x].rating == 2){
+      count_2 = count_2 + 1
+    }else if(result[x].rating == 3){
+      count_3 = count_3 + 1
+    }else if(result[x].rating == 4){
+      count_4 = count_4 + 1
+    }else if(result[x].rating == 5){
+      count_5 = count_5 + 1
+    }
+
+  }
+  let numOfRating = result.length;
+  ratingCount= [
+    {"1": count_1, 
+    "percent": Math.round((count_1*100)/numOfRating)},
+    {"2": count_2, 
+    "percent": Math.round((count_2*100)/numOfRating)},
+    {"3": count_3, 
+    "percent": Math.round((count_3*100)/numOfRating)},
+    {"4": count_4, 
+    "percent": Math.round((count_4*100)/numOfRating)},
+    {"5": count_5, 
+    "percent": Math.round((count_5*100)/numOfRating)},
+  ]
+  
+  let avgRating = sum / result.length
 
 //   let numOfReviews = Ratings.find({ review: { $exists: true, $ne: [] } })
   if(result.err){
@@ -66,7 +103,9 @@ exports.getRatings = catchAsync(async(req,res,next)=>{
       status: STATUS.SUCCESS,
       message: "Ratings of individual product",
       data: result,
-      numOfRatings: result.length,
+      numOfRatings: result.numOfRating,
       numOfReviews: result.length,
+      avgRating: Math.round(avgRating,2),
+      ratingCount: ratingCount
     });    
 })
